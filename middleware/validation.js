82da0cdec592bf697d6b59d86fbe109efad94de0
abdullah2403571-cell <abdullah2403571@miuegypt.edu.eg>
2026-06-07 10:31:42 +0300

    validation done

--- middleware/validation.js
+++ middleware/validation.js
@@ -2,12 +2,6 @@ const { body, param } = require('express-validator');
 
 // Signup form validation
 exports.validateSignup = [
-  body('
-
-    const { body, param } = require('express-validator');
-
-// Signup form validation
-exports.validateSignup = [
   body('name').trim().isLength({ min: 2, max: 50 }).withMessage('Name must be 2-50 characters'),
   body('email').isEmail().normalizeEmail().withMessage('Enter a valid email address'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
